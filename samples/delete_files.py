cf7f1cdc2ab108971130df00d285b49a153de2a6
Wanxin Li <w328li@uwaterloo.ca>
2020-08-22 17:45:32 +0800

    add delete_files.py

---
+++ samples/delete_files.py
@@ -0,0 +1,16 @@
+# argv[1]: directory to keep 
+# argv[2]: number of files to keep per dir
+
+import numpy as np
+import sys
+import os
+
+dir = sys.argv[1]
+final_num = int(sys.argv[2])
+
+for sub_dir in os.listdir(dir):
+    file_names = os.listdir(dir + "/"+sub_dir)
+    files_num = len(file_names)
+    selected_files = np.random.choice(file_names, final_num-files_num)
+    for file in selected_files:
+        os.remove(dir+"/"+sub_dir+"/"+file)
