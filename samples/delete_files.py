c8c9549ba039887fd3c568535e80290ceccd92b2
Wanxin Li <w328li@uwaterloo.ca>
2020-08-22 18:05:03 +0800

    updated delete_files.py

--- samples/delete_files.py
+++ samples/delete_files.py
@@ -9,9 +9,11 @@ dir = sys.argv[1]
 final_num = int(sys.argv[2])
 
 for sub_dir in os.listdir(dir):
-    file_names = os.listdir(dir + "/"+sub_dir)
+    file_names = os.listdir(dir+"/"+sub_dir)
     files_num = len(file_names)
     target_num = final_num-files_num
+    print(final_num)
+    print(files_num)
     if target_num <= 0:
         continue
     selected_files = np.random.choice(file_names, final_num-files_num)
