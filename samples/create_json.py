a12847315ab897e3a0adb65e5179a5c95c45e342
Wanxin Li <w328li@uwaterloo.ca>
2020-08-23 06:40:54 -0400

    updated create_json.py

--- samples/create_json.py
+++ samples/create_json.py
@@ -5,7 +5,7 @@ import json
 input_dir = sys.argv[1]
 input_base_path = '/home/w328li/MLDSP-desktop/samples/'
 input_dir_path = input_base_path+input_dir+"/"
-output_dir_path = '/home/w328li/BlindKameris/data/'
+output_dir_path = '/home/w328li/BlindKameris-desktop/data/'
 clusters = os.listdir(input_dir_path)
 dict_arr = []
 for c in clusters:
