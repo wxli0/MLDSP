907426807f4876edae256a2ea5ac35c34530267c
Wanxin Li <w328li@uwaterloo.ca>
2020-08-23 06:35:38 -0400

    updated create_kameris_input.py

--- samples/create_kameris_input.py
+++ samples/create_kameris_input.py
@@ -8,7 +8,7 @@ import glob
 input_folder = sys.argv[1]
 
 src = '/home/w328li/MLDSP-desktop/samples/'+input_folder
-dest = '/home/w328li/BlindKameris/data/'+input_folder
+dest = '/home/w328li/BlindKameris-desktop/data/'+input_folder
 
 if os.path.exists(dest):
     shutil.rmtree(dest, ignore_errors=True)
