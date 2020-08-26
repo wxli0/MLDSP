5ebb7111d7ae513f465435218c8ee025dcc76720
Wanxin Li <w328li@uwaterloo.ca>
2020-08-26 14:20:01 +0800

    added entries printing

--- printMisclassifiedEntriesCM.m
+++ printMisclassifiedEntriesCM.m
@@ -1,4 +1,6 @@
 function [] = printMisclassifiedEntriesCM(cm)
+    global falseEntries 
+    falseEntries = containers.Map;
     fprintf("printing misclassified entries\n")
     for i = 1:size(cm, 1)
         for j = 1:size(cm, 2)
@@ -6,8 +8,16 @@ function [] = printMisclassifiedEntriesCM(cm)
                 continue
             end
             if cm(i, j) ~= 0
-                fprintf("(%d,%d):%d\n", i, j, cm(i,j))
+                fprintf("(%d,%d):%d\n", i, j, cm(i,j));
+                global falseEntries
+                if isKey([num2str(i) num2str(j)], falseEntries)
+                    falseEntries([num2str(i) num2str(j)]) = [falseEntries([num2str(r) num2str(p)]) cm(i,j)];
+                else
+                    falseEntries([num2str(i) num2str(j)]) = [cm(i,j)];
+                end
             end
         end
     end
-        
+    fprintf("printing false entries\n");
+    disp(falseEntries);  
+end      
