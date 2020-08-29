06ac3697f8c4c656aefd124e380b4619ad894b9e
Wanxin Li <w328li@uwaterloo.ca>
2020-08-29 11:40:26 +0800

    updated count_files.py

--- samples/count_files.py
+++ samples/count_files.py
@@ -4,7 +4,7 @@ import platform
 
 folder = sys.argv[1]
 base_path = '/Users/wanxinli/Desktop/project/MLDSP-desktop/samples'
-if platform.platform() == 'Linux-5.3.0-61-generic-x86_64-with-debian-buster-sid':
+if platform.platform()[:5] == 'Linux':
     base_path = '/home/w328li/MLDSP-desktop/samples'
 for dir in os.listdir(base_path+'/'+folder):
     file_num = len(os.listdir(base_path+'/'+folder+'/'+dir))
