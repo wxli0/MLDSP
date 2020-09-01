e77768d8542d11a539be68af6778096a9b7c6c2f
Wanxin Li <w328li@uwaterloo.ca>
2020-09-01 17:38:48 +0800

    udpated count_files.py

--- samples/count_files.py
+++ samples/count_files.py
@@ -3,9 +3,12 @@ import sys
 import platform
 
 folder = sys.argv[1]
+total = 0
 base_path = '/Users/wanxinli/Desktop/project/MLDSP-desktop/samples'
 if platform.platform()[:5] == 'Linux':
     base_path = '/home/w328li/MLDSP-desktop/samples'
 for dir in os.listdir(base_path+'/'+folder):
     file_num = len(os.listdir(base_path+'/'+folder+'/'+dir))
     print(dir+":", file_num)
+    total += file_num
+print("total number:", total)
