093ad0a6934daecc96a9046be0ab282beae273cb
Wanxin Li <w328li@uwaterloo.ca>
2020-09-09 16:49:27 +0800

    added class seed 3

--- classificationCode.m
+++ classificationCode.m
@@ -10,8 +10,8 @@ function [ accuracy, avg_acc, clNames, cMat ] = classificationCode( disMat,alabe
     % Western University, Canada     %
     % email: grandha8@uwo.ca         %
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    seed = 98;
-    rng(seed);
+    % seed = 98;
+    % rng(seed);
     cv = cvpartition(totalSeq,'kfold',folds);
     cMat1=cell(1,folds);
     cMat2=cell(1,folds);
