222e6c5727b978484e7c202352ac26257e22d809
Wanxin Li <w328li@uwaterloo.ca>
2020-10-06 14:07:50 +0800

    added f__Acutalibacteraceae_exclude_g__Ruminococcus_E.json

--- data/DS_10283_3009/results_stats.py
+++ data/DS_10283_3009/results_stats.py
@@ -1,8 +1,9 @@
 import pandas as pd 
 import sys
+import operator
 
-one_taxon = sys.argv[1] # e.g. genus
-taxon_name = sys.argv[2] # e.g. g__Prevotella
+# one_taxon = sys.argv[1] # e.g. genus
+# taxon_name = sys.argv[2] # e.g. g__Prevotella
 results_table = pd.read_excel("paper_results.xlsx", sheet_name = "Table S5", skiprows = [0,1])
 results_table = results_table[['MAG', 'GTDB_Tk_classification']]
 taxon_num = 7
@@ -19,12 +20,25 @@ for i in range(len(taxon_names)):
 
 print(results_table)
 
-to_print = []
-for index, row in results_table.iterrows():
-    # print(row[taxon])
-    if row[one_taxon] == taxon_name:
-        to_print.append(row['MAG'])
+# to_print = []
+# for index, row in results_table.iterrows():
+#     # print(row[taxon])
+#     if row[one_taxon] == taxon_name:
+#         to_print.append(row['MAG'])
 
-print(to_print)
-print(len(to_print))
+# print(to_print)
+# print(len(to_print))
+
+print(results_table['genus'])
+dict = {}
+for s in list(results_table['genus']):
+    if s in dict:
+        dict[s] += 1
+    else:
+        dict[s] = 1
+
+sorted_dict = sorted(dict.items(), key=lambda x: x[1], reverse=True)
+print(sorted_dict)
+
+
 
