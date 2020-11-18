f01511283b368fb7717dc5b1b4ca8fcd31b5faf2
Wanxin Li <w328li@uwaterloo.ca>
2020-11-18 20:23:43 +0800

    debug'

--- classifyTestSeqExternMisList.m
+++ classifyTestSeqExternMisList.m
@@ -267,7 +267,7 @@ function [pMat,mList1,mList2,mList3,mList4,mList5,mList6] = classifyTestSeqExter
     disp([clusterNames score1Matrix])
     header = {'T1', 'T2', 'T3', 'prediction'};
     T = array2table(score1Matrix,'VariableNames',{'x_axis','y_axis','z_axis', 'prediction'})
-    writematrix(T,'M.xlsx','Sheet',1);  
+    writetable(T,'M.xlsx','Sheet',1);  
 
     % writematrix(score1Matrix, strcat("outputs/", dataSet, ".xls"), 'Sheet', 'linear-discriminant-score');  
     % writematrix(score2Matrix, strcat("outputs/", dataSet, ".xls"), 'Sheet', 'linear-svm-score');  
