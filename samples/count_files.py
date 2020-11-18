7cb43780787f70357ed1edd7bd473de91b265e6d
Wanxin Li <w328li@uwaterloo.ca>
2020-11-18 03:35:08 -0500

    changed path

--- samples/count_files.py
+++ samples/count_files.py
@@ -6,7 +6,7 @@ folder = sys.argv[1]
 total = 0
 base_path = '/Users/wanxinli/Desktop/project/MLDSP-desktop/samples'
 if platform.platform()[:5] == 'Linux':
-    base_path = '/home/w328li/MLDSP-desktop/samples'
+    base_path = '/home/w328li/MLDSP/samples'
 for dir in os.listdir(base_path+'/'+folder):
     file_num = len(os.listdir(base_path+'/'+folder+'/'+dir))
     print(dir+":", file_num)
