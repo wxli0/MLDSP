79e75c0ae5ba40ab93e8bff9f2c8f35bf3404396
Wanxin Li <w328li@uwaterloo.ca>
2021-01-18 20:18:32 +0800

    updated run_kameris_json.py

---
+++ samples/run_kameris_json.py
@@ -0,0 +1,10 @@
+import os
+
+files = ["f__Acutalibacteraceae_eval_wrapper",  "f__CAG-272_eval_wrapper", "f__Oscillospiraceae_eval_wrapper",
+"f__Butyricicoccaceae_eval_wrapper",   "f__CAG-382_eval_wrapper",  "f__Ruminococcaceae_eval_wrapper",]
+
+for file in files:
+    os.system("python3 create_kameris_input.py "+file)
+    os.system("python3 create_json.py "+file)
+
+
