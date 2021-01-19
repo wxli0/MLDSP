7f49a6a545aee57b0ffa07f805de20eaaed64c3a
Wanxin Li <w328li@uwaterloo.ca>
2021-01-19 10:16:04 +0800

    debug

--- samples/rename_eval.py
+++ samples/rename_eval.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from shutil import copyfile
+from distutils.dir_util import copy_tree
 
 # e.g. python3 
 
@@ -9,5 +10,6 @@ files = os.listdir(dir)
 
 for file in files:
     os.rename(os.path.join(dir, file), os.path.join(dir, file+'_eval'))
-    os.mkdir(file+'_eval_wrapper')
-    copyfile(os.path.join(dir, file+'_eval'), os.path.join(file+'_eval_wrapper', file+'_eval'))
+    if not os.path.exists(file+'_eval_wrapper'):
+        os.mkdir(file+'_eval_wrapper')
+    copy_tree(os.path.join(dir, file+'_eval'), os.path.join(file+'_eval_wrapper', file+'_eval'))
