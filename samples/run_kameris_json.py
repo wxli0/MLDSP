8795db0a2e6513cf144ada5d5782bd9383383a25
Wanxin Li <w328li@uwaterloo.ca>
2021-01-19 16:05:41 +0800

    debug

--- samples/run_kameris_json.py
+++ samples/run_kameris_json.py
@@ -5,7 +5,7 @@ dir = sys.argv[1]
 files = os.listdir(dir)
 
 for file in files:
-    file = file+"_eval_wrapper"
+    file = file+"_wrapper"
     os.system("python3 create_kameris_input.py "+file)
     os.system("python3 create_json.py "+file)
 
