f590c7d24483c35934be11e50b7efba2fe6640bc
Wanxin Li <w328li@uwaterloo.ca>
2021-03-08 12:12:58 +0800

    added dm_full.json

---
+++ outputs/gtdbtk/preprocess_tsv.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+
+cluster_tsv = pd.read_excel("/Users/wanxinli/Desktop/project/MLDSP-desktop/outputs/gtdbtk/GTDB-Tk_classification.xlsx", header = 0, index_col = 0)
+cluster_tsv['GTDB_taxonomy'] = cluster_tsv[["classification"]]
+
+
+taxon_names = ['domain', 'phylum', 'class', 'order', 'family', 'genus', 'species']
+for i in range(len(taxon_names)):
+    taxon  = taxon_names[i]
+    cluster_tsv[taxon] = cluster_tsv.apply(lambda row: row.GTDB_taxonomy.split(";")[i], axis=1)
+cluster_tsv = cluster_tsv.drop(columns=['GTDB_taxonomy'])
+# cluster_tsv.to_csv("gtdb-taxon.csv", index=True)
+
+print(cluster_tsv)
+
+print("printing all genera")
+print(sorted(list(set(cluster_tsv['genus'].tolist()))))
