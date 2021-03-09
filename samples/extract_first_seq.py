84c30ddc0d0246739c998aa072ac392cb710ee4a
Wanxin Li <w328li@uwaterloo.ca>
2021-03-09 23:49:07 +0800

    debug

--- samples/extract_first_seq.py
+++ samples/extract_first_seq.py
@@ -9,7 +9,7 @@ output_path = "/home/w328li/MLDSP/samples/rumen_mags/all_first/"
 for file in os.listdir(input_path):
     fasta_sequences = SeqIO.parse(open(input_path+file),'fasta') 
     output_file_path = output_path+file
-    out_file= open(output_path, 'w')
+    out_file= open(output_file_path, 'w')
     out_file.write(">"+fasta_sequences[0].id+"\n")
     out_file.write(fasta_sequences[0].seq+"\n")
     out_file.close()
