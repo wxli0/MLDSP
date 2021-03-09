7fd20c2b97be523c44458ae7cc0c2a7665af33e7
Wanxin Li <w328li@uwaterloo.ca>
2021-03-09 23:51:52 +0800

    debug

--- samples/extract_first_seq.py
+++ samples/extract_first_seq.py
@@ -10,6 +10,9 @@ for file in os.listdir(input_path):
     fasta_sequences = SeqIO.parse(open(input_path+file),'fasta') 
     output_file_path = output_path+file
     out_file= open(output_file_path, 'w')
-    out_file.write(">"+fasta_sequences[0].id+"\n")
-    out_file.write(fasta_sequences[0].seq+"\n")
-    out_file.close()
+    for fasta in fasta_sequences:
+        id, seq = fasta.id, str(fasta.seq)
+        out_file.write(">"+id+"\n")
+        out_file.write(seq+"\n")
+        out_file.close()
+        break
