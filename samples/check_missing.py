f6244316feb3540eee2544bd5b176f886d8cd59c
Wanxin Li <w328li@uwaterloo.ca>
2021-03-29 21:46:53 +0800

    debug

--- samples/check_missing.py
+++ samples/check_missing.py
@@ -12,8 +12,8 @@ print(len(S2["MAG_ID_ENA"]))
 
 all_files = os.listdir("/mnt/sda/MLDSP-samples/ERP108418")
 missing = []
-for file in all_files:
-    if file[:-6] not in S2["MAG_ID_ENA"]:
-        print(file + "missing")
+for mag in S2["MAG_ID_ENA"]:
+    if mag+".fa.gz" not in all_files:
+        print(mag+".fa.gz" + "missing")
         missing.append(file)
 print("length of missing files are:", len(missing))
