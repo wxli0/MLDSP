a666d6afb6506d9a3418b74df6488d3f02284b87
Wanxin Li <w328li@uwaterloo.ca>
2021-03-29 22:11:57 +0800

    debug

--- samples/check_missing.py
+++ samples/check_missing.py
@@ -10,15 +10,16 @@ S2 = pd.read_csv(true_path, skiprows=0, header=1)
 print("length of S2 is:", S2.shape[0])
 
 all_files = os.listdir("/mnt/sda/MLDSP-samples/ERP108418")
-print("length of all_files is:", len(all_files))
-missing = []
-for mag in S2["MAG_ID_ENA"]:
-    if mag+".fa.gz" not in all_files:
-        print(mag+".fa.gz" + " missing")
-        missing.append(mag+".fa.gz")
-print("length of missing files are:", len(missing))
+print(S2["MAG_ID_ENA"])
+# print("length of all_files is:", len(all_files))
+# missing = []
+# for mag in S2["MAG_ID_ENA"]:
+#     if mag+".fa.gz" not in all_files:
+#         print(mag+".fa.gz" + " missing")
+#         missing.append(mag+".fa.gz")
+# print("length of missing files are:", len(missing))
 
-for file in all_files:
-    print(file[:-6])
-    if file[:-6] not in S2["MAG_ID_ENA"]:
-        print(file+" redundant")
+# for file in all_files:
+#     print(file[:-6])
+#     if file[:-6] not in S2["MAG_ID_ENA"]:
+#         print(file+" redundant")
