4a3a0f0044cfdde73b79b97177c5751d6f44f305
Wanxin Li <w328li@uwaterloo.ca>
2021-04-05 20:53:02 +0800

    removed unused files, added make_HGR_training_from_species.py

---
+++ samples/make_HGR_training_from_species.py
@@ -0,0 +1,59 @@
+import os 
+import sys 
+import pandas as pd 
+import shutil
+
+# e.g. python3 make_HGR_training_set Phylum
+true_path = "Table_S1.csv"
+S1 = pd.read_csv(true_path, skiprows=0, header=1)
+print(S1)
+
+rank=sys.argv[1]
+pos = list(S1[rank])
+cleaned_pos = list(set([x for x in pos if str(x) != 'nan']))
+cleaned_pos.sort()
+# print(cleaned_pos)
+# print(list(set(cleaned_pos)).sort())
+
+next_rank_dict = {
+    'Phylum': 'Class',
+    'Class': 'Order',
+    'Order': 'Family',
+    'Family': 'Genus'
+}
+
+next_rank = next_rank_dict[rank]
+parent_child_dict = {}
+next_rank_child_dict = {}
+for item in cleaned_pos:
+    S1_subset = S1.loc[S1[rank] == item]
+    next_rank_pos = list(S1_subset[next_rank])
+    cleaned_next_rank_pos = list(set([x for x in next_rank_pos if str(x) != 'nan']))
+    cleaned_next_rank_pos.sort()
+    # print(item+":", cleaned_next_rank_pos)
+    parent_child_dict[item] = cleaned_next_rank_pos
+    for next_rank_item in next_rank_pos:
+        S1_subset_next_rank = S1.loc[S1[next_rank] == next_rank_item]
+        next_rank_genome_list = list(S1_subset_next_rank['Genome'])
+        cleaned_next_rank_genome = list(set([x for x in next_rank_genome_list if str(x) != 'nan']))
+        cleaned_next_rank_genome.sort()
+        next_rank_child_dict[next_rank_item] = cleaned_next_rank_genome
+
+
+print(parent_child_dict)
+print(next_rank_child_dict)
+parent_prefix = rank[0].lower()+"__"
+child_prefix = next_rank[0].lower()+"__"
+for parent, children in parent_child_dict.items():
+    os.mkdir("labeled_genome/"+parent_prefix+parent)
+    if len(children) == 0:
+        continue
+    for child in children:
+        pos_genome = next_rank_child_dict[child]
+        if len(pos_genome) == 0:
+            continue 
+        os.mkdir("labeled_genome/"+parent_prefix+parent+"/"+child_prefix+child)
+        for genome in pos_genome:
+            if os.path.isfile("labeled_genome/label_"+genome+".fa"):
+                shutil.copyfile("labeled_genome/label_"+genome+".fa", \
+                    "labeled_genome/"+parent_prefix+parent+"/"+child_prefix+child+"/"+"label_"+genome+".fa")
