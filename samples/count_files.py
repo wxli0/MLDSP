0c1b88e587d526ce1edb8355d844bbe44c994c68
Wanxin Li <w328li@uwaterloo.ca>
2021-04-15 17:32:36 +0800

    updated count_files.py

--- samples/count_files.py
+++ samples/count_files.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import platform
+from Bio import SeqIO
+
 
 folder = sys.argv[1]
 total = 0
@@ -9,6 +11,13 @@ if platform.platform()[:5] == 'Linux':
     base_path = '/home/w328li/MLDSP/samples'
 for dir in os.listdir(base_path+'/'+folder):
     file_num = len(os.listdir(base_path+'/'+folder+'/'+dir))
-    print(dir+":", file_num)
+    total_len = 0
     total += file_num
+    for file in os.listdir(base_path+'/'+folder+'/'+dir):
+        fasta_sequences = SeqIO.parse(file, "fasta")
+        for fasta in fasta_sequences:
+            _, sequence = fasta.id, str(fasta.seq)
+            total_len += len(sequence)
+            count += 1
+    print(dir+":", file_num, "and total_len:", total_len)
 print("total number:", total)
