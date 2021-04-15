93033090d1415e7885908e9b03980ea356c7c8cf
Wanxin Li <w328li@uwaterloo.ca>
2021-04-15 17:37:18 +0800

    udpated count files

--- samples/count_files.py
+++ samples/count_files.py
@@ -6,15 +6,15 @@ from Bio import SeqIO
 
 folder = sys.argv[1]
 total = 0
-base_path = '/Users/wanxinli/Desktop/project/MLDSP-desktop/samples'
+base_path = '.'
 if platform.platform()[:5] == 'Linux':
-    base_path = '/home/w328li/MLDSP/samples'
+    base_path = '.'
 for dir in os.listdir(base_path+'/'+folder):
     file_num = len(os.listdir(base_path+'/'+folder+'/'+dir))
     total_len = 0
     total += file_num
     for file in os.listdir(base_path+'/'+folder+'/'+dir):
-        fasta_sequences = SeqIO.parse(file, "fasta")
+        fasta_sequences = SeqIO.parse(os.path.join(base_path, dir, file), "fasta")
         for fasta in fasta_sequences:
             _, sequence = fasta.id, str(fasta.seq)
             total_len += len(sequence)
