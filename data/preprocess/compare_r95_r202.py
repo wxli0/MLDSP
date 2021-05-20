6118afe25ffaf186383892e1f7022973d16440fe
Wanxin Li <w328li@uwaterloo.ca>
2021-05-20 22:43:49 +0800

    updated r95_r202_diff.log

--- data/preprocess/compare_r95_r202.py
+++ data/preprocess/compare_r95_r202.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 r95 = pd.read_csv("sp_clusters_r95.tsv", sep='\t', header = 0, index_col = 0)
 r202 = pd.read_csv("sp_clusters_r202.tsv", sep='\t', header = 0, index_col = 0)
-print("checking representative genome changes in r95 and r202")
+print("==== checking representative genome changes between r95 and r202 ====")
 
 for index, row in r95.iterrows():
     if index not in r202.index:
@@ -13,5 +13,5 @@ for index, row in r95.iterrows():
     r202_row = r202.loc[index]
     if r202_row['GTDB_species'] != row['GTDB_species']:
         print(index+":", "FROM")
-        print(row['GTDB_taxonomy']+";"+row['GTDB_species'], "TO")
-        print(r202_row['GTDB_taxonomy']+";"+r202_row['GTDB_species'])
+        print("r95 :", row['GTDB_taxonomy']+";"+row['GTDB_species'], "TO")
+        print("r202:", r202_row['GTDB_taxonomy']+";"+r202_row['GTDB_species'])
