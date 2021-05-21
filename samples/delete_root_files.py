fc5b15345c406796891cd66da53d689b011e9632
Wanxin Li <w328li@uwaterloo.ca>
2021-05-22 00:22:14 +0800

    debug

--- samples/delete_root_files.py
+++ samples/delete_root_files.py
@@ -6,7 +6,7 @@ dir = sys.argv[1]
 ver = sys.argv[2]
 if dir == 'root':
     bac_files = os.listdir('/mnt/sda/MLDSP-samples-'+ver+'/root/d__Bacteria')
-    delete_bac = np.random.choice(bac_files, int(len(bac_files)*0.9))
+    delete_bac = np.random.choice(bac_files, int(len(bac_files)*0.9), replace=False)
     for file in delete_bac:
         print(file)
         os.remove('/mnt/sda/MLDSP-samples-'+ver+'/root/d__Bacteria/'+file)
@@ -14,4 +14,4 @@ if dir == 'root':
     arc_files = os.listdir('/mnt/sda/MLDSP-samples-'+ver+'/root/d__Archaea')
     delete_arc = np.random.choice(arc_files, int(len(arc_files)*0.9))
     for file in delete_arc:
-        os.remove('/mnt/sda/MLDSP-samples-'+ver+'/root/d__Archaea/'+file)
+        os.remove('/mnt/sda/MLDSP-samples-'+ver+'/root/d__Archaea/'+file, replace=False)
