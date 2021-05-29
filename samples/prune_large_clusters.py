d858fe4031f35c057b34d59099b954c3bb3ecee9
Wanxin Li <w328li@uwaterloo.ca>
2021-05-29 22:58:58 +0800

    added p__Firmicutes to prune_large_clusters.py

--- samples/prune_large_clusters.py
+++ samples/prune_large_clusters.py
@@ -15,6 +15,7 @@ if dir == 'root':
     for file in delete_arc:
         os.remove('/mnt/sda/MLDSP-samples-'+ver+'/root/d__Archaea/'+file)
 if dir.startswith('d__') or dir == 'p__Actinobacteriota' or dir == 'p__Bacteroidota' \
+    or dir == 'p__Firmicutes' \
     or dir == 'p__Firmicutes_A' or dir == 'p__Cyanobacteria' or dir == 'c__Actinomycetia' \
         or dir == 'c__Bacteroidia' or dir == 'c__Clostridia':
     arc_dirs = os.listdir('/mnt/sda/MLDSP-samples-'+ver+'/'+dir)
