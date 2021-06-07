ad6ebf0ebd18647756b48d35026065e1a605db3c
Wanxin Li <w328li@uwaterloo.ca>
2021-06-07 11:13:11 +0800

    debugged phase_HGR.sh

--- phase_HGR.sh
+++ phase_HGR.sh
@@ -1,34 +1,41 @@
 #!/bin/bash
 
 ver="r202"
-base_dir="DeepMicrobes-data/labeled_genome-${ver}/"
-sample_dir="${base_dir}$1"
+base_dir="/mnt/sda/DeepMicrobes-data/labeled_genome-${ver}/"
 outdir="outputs-HGR-${ver}"
 testdir="hgr_mags"
+split_pruned_dir="${base_dir}$1_split_pruned"
+
+if [ ! -d ${split_pruned_dir} ]; then
+    python3 ~/DeepMicrobes/scripts/split_fasta_5000.py ${split_pruned_dir}
+    echo "INFO: done python3 ~/DeepMicrobes/scripts/split_fasta_5000.py ${split_pruned_dir}"
+else
+    echo "INFO: skip python3 ~/DeepMicrobes/scripts/split_fasta_5000.py ${split_pruned_dir}"
+fi
 
 prog_output1="${outdir}/train-$1.xlsx"
 if [ ! -f ${prog_output1} ]; then
     output1="${outdir}/$1.txt"
-    matlab -r "run addme;stackedMain('HGR', '$1');exit"|tee ${output1}
-    echo "INFO:done stackedMain('HGR', '$1')"
+    matlab -r "run addme;stackedMain('HGR', '${split_pruned_dir}');exit"|tee ${output1}
+    echo "INFO:done stackedMain('HGR', '${split_pruned_dir}')"
 else
-    echo "INFO:skip stackedMain('HGR', '$1')"
+    echo "INFO:skip stackedMain('HGR', '${split_pruned_dir}')"
 fi
 
 prog_output2="${outdir}/test-$1.xlsx"
 if [ ! -f ${prog_output2} ]; then
     output2="${outdir}/$1_classify.txt"
-    matlab -r "run addme;stackedMain('HGR', '$1', '${testdir}/$1');exit"|tee ${output2}
-    echo "INFO:done stackedMain('HGR', '$1', '${testdir}/$1')"
+    matlab -r "run addme;stackedMain('HGR', '${split_pruned_dir}', '${testdir}/$1');exit"|tee ${output2}
+    echo "INFO:done stackedMain('HGR', '${split_pruned_dir}', '${testdir}/$1')"
 else
-    echo "INFO:skip stackedMain('HGR', '$1', '${testdir}/$1')"
+    echo "INFO:skip stackedMain('HGR', '${split_pruned_dir}', '${testdir}/$1')"
 fi
 
 dir="/home/w328li/BlindKameris-new/"
 cd ${dir}
 echo "INFO: done cd ${dir}"
 
-src1="/home/w328li/MLDSP/${outdir}/train-$1.xlsx"
+src1="/home/w328li/MLDSP/${outdir}/train-${split_pruned_dir}.xlsx"
 dest1="${outdir}/$1_train.xlsx"
 cp ${src1} ${dest1}
 echo "INFO:done cp ${src1} ${dest1}"
@@ -39,7 +46,7 @@ echo "INFO:done preprocess_train_to_pr.py ${dest1}"
 python3 run_precision_recall_b_taxon.py ${dest1}
 echo "INFO:done run_precision_recall_b_taxon.py ${dest1}"
 
-src2="/home/w328li/MLDSP/${outdir}/test-$1.xlsx"
+src2="/home/w328li/MLDSP/${outdir}/test-${split_pruned_dir}.xlsx"
 dest2="/home/w328li/BlindKameris-new/${outdir}/$1.xlsx"
 cp ${src2} ${dest2}
 echo "INFO:done cp ${src2} ${dest2}"
