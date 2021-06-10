4968ea715148d427d5f9ad15bbb525780dba37aa
Wanxin Li <w328li@uwaterloo.ca>
2021-06-10 18:26:46 +0800

    debug

--- stackedMain.m
+++ stackedMain.m
@@ -33,7 +33,7 @@ function [] = stackedMain(dataType, dataSet, testSet)
     allCGR = cell(1,totalSeq);
 
     fprintf("totalSeq is: %d\n", totalSeq)
-    parfor i=1:totalSeq
+    for i=1:totalSeq
         ss = Seq{i};
         tCGR=zeros(2^kVal);
         for j=1:length(ss)
