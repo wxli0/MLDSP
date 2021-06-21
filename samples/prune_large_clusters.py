4d08edb21d0e6a89469bb327a9e69e77e1e7441e
Wanxin Li <w328li@uwaterloo.ca>
2021-06-21 13:56:21 +0800

    added f__Lachnospiraceae to large_clusters

--- samples/prune_large_clusters.py
+++ samples/prune_large_clusters.py
@@ -36,7 +36,7 @@ elif data_type == 'HGR':
             or dir.startswith('c__Brachyspirae') or dir.startswith('p__Actinobacteriota') \
                 or dir.startswith('c__Synergistia') or dir.startswith('c__Coriobacteriia') \
                     or dir.startswith('o__Oscillospirales') or dir.startswith('o__Coriobacteriales') \
-                        or dir.startswith('f__Bacteroidaceae'):
+                        or dir.startswith('f__Bacteroidaceae') or dir.startswith('f__Lachnospiraceae'):
         arc_dirs = os.listdir('/mnt/sda/DeepMicrobes-data/labeled_genome-'+ver+'/'+dir)
         for d in arc_dirs:
             all_files = os.listdir('/mnt/sda/DeepMicrobes-data/labeled_genome-'+ver+'/'+dir+'/'+d)
