aa5364f1297e0a3a401d0edceb7134fd316a2db2
Wanxin Li <w328li@uwaterloo.ca>
2021-06-21 21:17:27 +0800

    debug

--- samples/check_missing_dm_species.py
+++ samples/check_missing_dm_species.py
@@ -6,7 +6,7 @@ dir = '/mnt/sda/MLDSP-samples-r202/dm_species/'
 MLDSP_pred_path = '/home/w328li/BlindKameris-new/outputs-r202/MLDSP-prediction-full-path.csv'
 
 MLDSP_df =  pd.read_csv(MLDSP_pred_path, index_col=0, header=0, dtype = str)
-MLDSP_species = MLDSP_df['gtdb-tk-species']
+MLDSP_species = list(set(MLDSP_df['gtdb-tk-species']))
 
 for file in os.listdir(dir):
     index = file.split('_combined.fna')[0]
