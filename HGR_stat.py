446561e36de1ed19e4442ba4b25444e4a8f563c1
Wanxin Li <w328li@uwaterloo.ca>
2021-06-24 00:41:50 +0800

    debug

--- HGR_stat.py
+++ HGR_stat.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 
 # compute taxon overview
-S1_path = '/home/w328li/MLDSP-desktop/samples/Table_S1_new.csv'
+S1_path = '/home/w328li/MLDSP/samples/Table_S1_new.csv'
 S1 = pd.read_csv(S1_path, skiprows=0, header=0)
 ranks = ['Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']
 for rank in ranks:
