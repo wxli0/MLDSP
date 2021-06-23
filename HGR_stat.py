6053e781f4e2580e4e387b8a0b5e76648323bbed
Wanxin Li <w328li@uwaterloo.ca>
2021-06-24 00:41:00 +0800

    added HGR_stat.py

---
+++ HGR_stat.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+
+# compute taxon overview
+S1_path = '/home/w328li/MLDSP-desktop/samples/Table_S1_new.csv'
+S1 = pd.read_csv(S1_path, skiprows=0, header=0)
+ranks = ['Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']
+for rank in ranks:
+    num = len(set(S1[rank]))
+    print(rank+":", num)
+
+
+
