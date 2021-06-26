879d220acf4724d1d634498ddf69f00ea24aa914
Wanxin Li <w328li@uwaterloo.ca>
2021-06-26 12:27:40 +0800

    added timing

--- phase_HGR.sh
+++ phase_HGR.sh
@@ -45,6 +45,7 @@ else
     echo "INFO: skip python3 samples/prune_large_clusters.py $split_pruned_dir $ver HGR"
 fi
 
+start_time1="$(date -u +%s)"
 prog_output1="${outdir}/train-${split_pruned_dir}.xlsx"
 if [ ! -f ${prog_output1} ]; then
     output1="${outdir}/$1.txt"
@@ -53,7 +54,11 @@ if [ ! -f ${prog_output1} ]; then
 else
     echo "INFO:skip stackedMain('HGR', '${split_pruned_dir}')"
 fi
+end_time1="$(date -u +%s)"
+elapsed1="$(($end_time1-$start_time1))"
+echo "$1 ${elapsed1}" >> "${outdir}/train_time.txt"
 
+start_time2="$(date -u +%s)"
 prog_output2="${outdir}/test-${split_pruned_dir}.xlsx"
 if [ ! -f ${prog_output2} ]; then
     output2="${outdir}/$1_classify.txt"
@@ -62,11 +67,15 @@ if [ ! -f ${prog_output2} ]; then
 else
     echo "INFO:skip stackedMain('HGR', '${split_pruned_dir}', '${testdir}/$1')"
 fi
+end_time2="$(date -u +%s)"
+elapsed2="$(($end_time2-$start_time2))"
+echo "$1 ${elapsed2}" >> "${outdir}/test_time.txt"
 
 dir="/home/w328li/BlindKameris-new/"
 cd ${dir}
 echo "INFO: done cd ${dir}"
 
+start_time3="$(date -u +%s)"
 src1="/home/w328li/MLDSP/${outdir}/train-${split_pruned_dir}.xlsx"
 dest1="${outdir}/$1_train.xlsx"
 if [ ! -f ${dest1} ]; then
@@ -86,7 +95,11 @@ echo "INFO:done cp ${src2} ${dest2}"
 
 python3 precision_recall.py ${dest1} ${dest2} "HGR"
 echo "INFO:done precision_recall.py ${dest1} ${dest2} HGR"
+end_time3="$(date -u +%s)"
+elapsed3="$(($end_time3-$start_time3))"
+echo "$1 ${elapsed3}" >> "${outdir}/rej_time.txt"
 
+start_time4="$(date -u +%s)"
 output3="${outdir}/$1.xlsx"
 rej="rejection-threshold-HGR-${ver}/$1.json"
 python3 preprocess_test.py ${output3} ${rej}
@@ -95,3 +108,6 @@ echo "INFO:done preprocess_test.py ${output3} ${rej}"
 
 python3 add_HGR_pred.py ${output3}
 echo "INFO: done add_HGR_pred.py ${output3}"
+end_time4="$(date -u +%s)"
+elapsed4="$(($end_time4-$start_time4))"
+echo "$1 ${elapsed4}" >> "${outdir}/post_time.txt"
