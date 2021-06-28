12e8e004758577b751c455813948dc30cb237fb9
Wanxin Li <w328li@uwaterloo.ca>
2021-06-28 12:12:01 +0800

    updated phase_hgr.sh

--- phase_HGR.sh
+++ phase_HGR.sh
@@ -79,39 +79,40 @@ dir="/home/w328li/BlindKameris-new/"
 cd ${dir}
 echo "INFO: done cd ${dir}"
 
-start_time3="$(date -u +%s)"
-src1="/home/w328li/MLDSP/${outdir}/train-${split_pruned_dir}.xlsx"
-dest1="${outdir}/$1_train.xlsx"
-if [ ! -f ${dest1} ]; then
-    cp ${src1} ${dest1}
-    echo "INFO:done cp ${src1} ${dest1}"
-    python3 preprocess_train_to_pr.py ${dest1}
-    echo "INFO:done preprocess_train_to_pr.py ${dest1}"
-else
-    echo "INFO:skip cp ${src1} ${dest1}"
-    echo "INFO:skip preprocess_train_to_pr.py ${dest1}"
-fi
+# start_time3="$(date -u +%s)"
+# src1="/home/w328li/MLDSP/${outdir}/train-${split_pruned_dir}.xlsx"
+# dest1="${outdir}/$1_train.xlsx"
+# if [ ! -f ${dest1} ]; then
+#     cp ${src1} ${dest1}
+#     echo "INFO:done cp ${src1} ${dest1}"
+#     python3 preprocess_train_to_pr.py ${dest1}
+#     echo "INFO:done preprocess_train_to_pr.py ${dest1}"
+# else
+#     echo "INFO:skip cp ${src1} ${dest1}"
+#     echo "INFO:skip preprocess_train_to_pr.py ${dest1}"
+# fi
 
 src2="/home/w328li/MLDSP/${outdir}/test-${split_pruned_dir}.xlsx"
 dest2="/home/w328li/BlindKameris-new/${outdir}/$1.xlsx"
 cp ${src2} ${dest2}
 echo "INFO:done cp ${src2} ${dest2}"
 
-python3 precision_recall.py ${dest1} ${dest2} "HGR"
-echo "INFO:done precision_recall.py ${dest1} ${dest2} HGR"
-end_time3="$(date -u +%s)"
-elapsed3="$(($end_time3-$start_time3))"
-echo "$1 ${elapsed3}" >> "${outdir}/rej_time.txt"
+# python3 precision_recall.py ${dest1} ${dest2} "HGR"
+# echo "INFO:done precision_recall.py ${dest1} ${dest2} HGR"
+# end_time3="$(date -u +%s)"
+# elapsed3="$(($end_time3-$start_time3))"
+# echo "$1 ${elapsed3}" >> "${outdir}/rej_time.txt"
 
-start_time4="$(date -u +%s)"
+# start_time4="$(date -u +%s)"
 output3="${outdir}/$1.xlsx"
-rej="rejection-threshold-HGR-${ver}/$1.json"
-python3 preprocess_test.py ${output3} ${rej}
-echo "INFO:done preprocess_test.py ${output3} ${rej}"
-
+# rej="rejection-threshold-HGR-${ver}/$1.json"
+# python3 preprocess_test.py ${output3} ${rej}
+# echo "INFO:done preprocess_test.py ${output3} ${rej}"
+python3 preprocess_test.py ${output3}
+echo "INFO:done preprocess_test.py ${output3}"
 
 python3 add_HGR_pred.py ${output3}
 echo "INFO: done add_HGR_pred.py ${output3}"
-end_time4="$(date -u +%s)"
-elapsed4="$(($end_time4-$start_time4))"
-echo "$1 ${elapsed4}" >> "${outdir}/post_time.txt"
+# end_time4="$(date -u +%s)"
+# elapsed4="$(($end_time4-$start_time4))"
+# echo "$1 ${elapsed4}" >> "${outdir}/post_time.txt"
