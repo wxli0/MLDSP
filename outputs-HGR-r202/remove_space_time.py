9d7b97996eaa22338e9ec17db68117f3fa3e629c
Wanxin Li <w328li@uwaterloo.ca>
2021-07-15 12:16:14 +0800

    removed useless rows in time.txt

---
+++ outputs-HGR-r202/remove_space_time.py
@@ -0,0 +1,4 @@
+text_file = open("sample.txt", "w")
+with open('/Users/wanxinli/Desktop/project.nosync/MLDSP-desktop/outputs-HGR-r202/train_time.txt') as f:
+    output = "".join(line for line in f if not line.isspace())
+text_file.write(output)
