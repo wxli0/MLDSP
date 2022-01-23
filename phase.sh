3f4b8e01325001f7fb8669d20a8817cbcc91ce7c
Wanxin Li <w328li@uwaterloo.ca>
2022-01-23 11:27:11 +0800

    undo last commit

--- phase.sh
+++ phase.sh
@@ -29,11 +29,9 @@ while getopts ":d:b:s:t:a:v:p" o; do
             test_dir=${OPTARG}
             ;;
         a)
-            accepted_CA=0.9
             accepted_CA=${OPTARG}
             ;;
         v)
-            variability=0.2
             variability=${OPTARG}
             ;;
         p)
