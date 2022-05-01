8489f738e80a6eeede95b915a7df41e527d2ee1d
Wanxin Li <w328li@uwaterloo.ca>
2022-05-01 09:52:57 +0800

    cleared unused files, ready for submission

--- outputs-HGR-r202-archive/remove_space_time.py
+++
@@ -1,4 +0,0 @@
-text_file = open("sample.txt", "w")
-with open('/Users/wanxinli/Desktop/project.nosync/MLDSP/outputs-HGR-r202/train_time.txt') as f:
-    output = "".join(line for line in f if not line.isspace())
-text_file.write(output)
